eff3d8305fe0eb3b88fe187af854b847d7919bc7
LikeSum <likesum96@gmail.com>
2019-10-28 14:40:58 -0500

    typo

--- face_deblur/scripts/download_kernels.sh
+++ face_deblur/scripts/download_kernels.sh
@@ -2,5 +2,5 @@
 
 # Download blur kernels
 wget https://github.com/likesum/unsupimg/releases/download/v1.0/blur_kernels.zip
-unzip blur_kernels.zip data/kernels -d data
+unzip blur_kernels.zip -d data
 rm blur_kernels.zip
